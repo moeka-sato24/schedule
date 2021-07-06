819885632a9ed99e4c649e7c43b512c509384333
関 直哉 <nyseki@users.noreply.github.com>
2021-07-06 14:54:28 +0900

    add Test

---
+++ Test/ScheduleTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+public class ScheduleTest {
+
+    @org.junit.Test
+    public void setScheduleListテスト() {
+
+    }
+
+    @org.junit.Test
+    public void getScheduleListテスト() {
+    }
+
+    @org.junit.Test
+    public void removeテスト() {
+    }
+
+    @org.junit.Test
+    public void displayテスト() {
+    }
+
+
+
+
+
+}
