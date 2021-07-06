0bd9d6274df5dea7c6d6295dcc1d6fdb3f9c6e39
関 直哉 <nyseki@users.noreply.github.com>
2021-07-06 15:51:37 +0900

    modify Test/ScheduleTest.java

--- Test/ScheduleTest.java
+++ Test/ScheduleTest.java
@@ -1,20 +1,51 @@
+import org.junit.Before;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 
 public class ScheduleTest {
+    public Schedule schedule;
 
     @Before
-    Schedule schedule;
+    public void createInstance() {
+        schedule = new Schedule();
+    }
+
 
     @org.junit.Test
-    public void setScheduleListテスト() {
+    public void setScheduleList_notNullテスト() {
+        schedule.setScheduleList(0, 1,"Test");
+        schedule.setScheduleList(0, 1,"NewTest");
+        assertThat(schedule.scheduleListAM.get(0), is("Test"));
     }
 
     @org.junit.Test
-    public void getScheduleListテスト() {
+    public void setScheduleList_setValueテスト() {
+        schedule.setScheduleList(0, 1,"Test");
+        assertThat(schedule.scheduleListAM.get(0), is("Test"));
     }
 
     @org.junit.Test
-    public void removeテスト() {
+    public void getScheduleList_nullテスト() {
+        assertThat(schedule.getScheduleList(0, 0), nullValue());
+    }
+
+    @org.junit.Test
+    public void getScheduleList_getValueテスト() {
+        schedule.scheduleListAM.set(0, "Test");
+        assertThat(schedule.getScheduleList(0, 0), nullValue());
+    }
+
+    @org.junit.Test
+    public void remove_Nullテスト() {
+        assertThat(schedule.remove(0, 0), nullValue());
+    }
+
+    @org.junit.Test
+    public void remove_removeValueテスト() {
+        schedule.scheduleListAM.set(0, "Test");
+        assertThat(schedule.remove(0, 0), nullValue());
     }
 
     @org.junit.Test
